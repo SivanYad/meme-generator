2ab5c5f08ecd8ed70b9cf669cf54f3615d5a88de
unknown <justsivan@gmail.com>
2022-06-15 13:07:40 +0300

    Added renderMeme works

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,12 +1,13 @@
-var elCanvas = document.querySelector('#my-canvas')
-var gCtx = elCanvas.getContext('2d');
+var gElCanvas = document.querySelector('#my-canvas')
+var gCtx = gElCanvas.getContext('2d');
 
-
+renderMeme()
 
 function renderMeme() {
     var img = new Image()
     img.src = getImageById().url
-    // console.log(img.src)
+    console.log(img.src)
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height) //img,x,y,xend,yend
+    }
 }
-
-getImageById
